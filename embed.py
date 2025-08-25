389c484475dff93d465a61cccf8e0a7a5db6f876
Celsius273 <krp.gm2846297@protonmail.com>
2025-08-24 17:53:04 -0700

    Aug 16 update

--- embed.py
+++ embed.py
@@ -1,29 +1,13 @@
 import json
-
 import numpy as np
 import faiss
 from sentence_transformers import SentenceTransformer
 
 def main():
-    # sentences = ["This is an example sentence", "Each sentence is converted"]
-    # model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
-    #
-    # embeddings = model.encode(sentences)
-    #
-    # embeddings = np.array(embeddings).astype('float32')
-    # index = faiss.IndexFlatL2(embeddings.shape[1])
-    # index.add(embeddings)
-    # print(index)
-    # faiss.write_index(index, 'data/vector.index')
-    # print("Number of vectors :", index.ntotal)
-    #
-    # k = 2  # Number of nearest neighbors to search for
-    # D, I = index.search(embeddings, k)  # Search for the embeddings themselves
-    # print("Distances:\n", D)
-    # print("Indices:\n", I)
     with open('data/articles.json') as f:
         articles_json = json.load(f)
-        model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+
+    model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
     for articles in articles_json:
         sentences = [articles['text'] for articles in articles_json]
         embeddings = model.encode(sentences)
@@ -35,18 +19,6 @@ def main():
 
         faiss.write_index(index, 'data/vector.index')
         print("Number of vectors :", index.ntotal)
-
-    metadata_dict = {}
-    for idx, article in enumerate(articles_json):
-        metadata_dict[idx] = {
-            "title": article['title'],
-            "date": article['date'],
-            "url": article['url'],
-            "publisher": article['publisher']
-        }
-    with open('data/metadata.json', 'w') as m_file:
-        json.dump(metadata_dict, m_file, indent=2)
-    print("dumped meta data")
 
 
 if __name__ == "__main__":
--- preprocess.py
+++ preprocess.py
@@ -13,25 +13,26 @@ def chunker():
             separators=["\n\n", "\n", ".", " ", ""]
         )
 
-        structured_chunks = []
+        structured_chunks = {}
+        c_id = 0
+
         for article in articles_json:
             raw_text = article["text"]
-
             chunks = splitter.split_text(raw_text)
 
-            for i, chunk in enumerate(chunks):
-                structured_chunks.append({
-                    "chunk_id": f"{article['title'][:30]}_{i}",  # unique id
+            for chunk in chunks:
+                structured_chunks[c_id] = {
                     "text": chunk,
                     "publisher": article["publisher"],
                     "date": article["date"],
                     "url": article["url"],
                     "title": article["title"]
-                })
+                }
+                c_id = c_id + 1
 
         output_file_path = 'data/chunkedArticles.json'
         with open(output_file_path, 'w') as fd:
-            json.dump({"chunked": structured_chunks}, fd, indent=4)
+            json.dump({"chunked": structured_chunks}, fd, indent=2)
         print(f"Data written to JSON file successfully at {output_file_path}.")
 
     except Exception as e:
