912f38a08e6e8e34d0b2e890655514450e10433c
Celsius273 <krp.gm2846297@protonmail.com>
2025-08-30 12:12:44 -0700

    Aug 30 combo of process & embed now faster

---
+++ process.py
@@ -0,0 +1,59 @@
+import json
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import numpy as np
+import faiss
+from sentence_transformers import SentenceTransformer
+
+
+
+def chunkandvect():
+    try:
+        with open('data/articles.json') as f:
+            articles_json = json.load(f)
+        model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+        index = None
+        splitter = RecursiveCharacterTextSplitter(
+            chunk_size=600,  # ~800 characters max per chunk
+            chunk_overlap=100,  # overlap so context carries over
+            separators=["\n\n", "\n", " ",]
+        )
+
+        c_id = 0
+        metadata = []
+        chunklist = []
+        for article in articles_json:
+            raw_text = article["text"]
+            chunks = splitter.split_text(raw_text)
+
+            for chunk in chunks:
+                meta = {"ID" : c_id, "text": chunk, "publisher": article["publisher"], "date": article["date"], "url": article["url"], "title": article["title"] }
+                c_id = c_id + 1
+                metadata.append(meta)
+                chunklist.append(chunk)
+
+            embeddings = model.encode(chunklist)
+            embeddings = np.array(embeddings).astype('float32')
+            if index is None:
+                index = faiss.IndexFlatL2(embeddings.shape[1])
+            index.add(embeddings)
+            chunklist.clear()
+
+
+        print("Number of vectors:", index.ntotal)
+        faiss.write_index(index, 'data/vector.index')
+        print("Number of vectors:", index.ntotal)
+        print("Num of chunks", len(metadata))
+        output_file_path = 'data/metadata.json'
+        with open(output_file_path, 'w') as fd:
+            json.dump({"chunked": metadata}, fd, indent=2)
+        print(f"Data written to JSON file successfully at {output_file_path}.")
+
+    except Exception as e:
+        print(f"Error writing to JSON file: {e}")
+
+
+def main():
+    chunkandvect()
+
+if __name__ == "__main__":
+    main()
